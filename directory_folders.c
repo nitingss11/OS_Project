c204739af266e1e8fa5a4451a1ab7d7f75c5bb1b
mahendra kumar <mahendra.dakshana15@gmail.com>
2017-05-02 16:42:24 +0530

    Add files via upload

---
+++ directory_folders.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main(void)
+{
+ FILE* fp;
+ char* command;
+ char* line;
+
+ line = malloc(200 * sizeof(char));
+ command = malloc(200 * sizeof(char));
+ strcpy(command,"ls");
+
+ fp = popen(command,"r");
+ while((fgets(line,  INT_MAX, fp)))
+ {
+  printf("%s \n",line);
+ }
+
+ return 0;
+}
+
