fe05deb047547c4d933a19b00d12dc998436b9dd
mahendra kumar <mahendra.dakshana15@gmail.com>
2017-05-03 22:48:26 +0530

    Add files via upload

---
+++ substr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+//*******************************************
+const char *substr(char *str, int left)
+{
+char substring[strlen(str)-left+1];
+  int c=left;
+int sub_i=0;
+ while (c <strlen(str)) {
+      substring[sub_i] = str[c];
+     c++;
+     sub_i++;
+   }
+   substring[sub_i] = '\0';
+   printf("%s",substring);
+
+return substring;
+}
+
+//*******************************************
+ 
+int main() 
+{char *s="<<Myname";
+printf("%s",substr(s,2));
+printf("\n%s",s);
+   return 0;
+
+}
