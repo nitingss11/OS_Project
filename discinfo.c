f362184a0c62f67663ac5fe064ecc3a02c984ce7
mahendra kumar <mahendra.dakshana15@gmail.com>
2017-05-03 14:26:30 +0530

    Update discinfo.c

--- discinfo.c
+++ discinfo.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <assert.h>
+#include <string.h>
+char** str_split(char* a_str, const char a_delim)
+{
+    char** result    = 0;
+    size_t count     = 0;
+    char* tmp        = a_str;
+    char* last_comma = 0;
+    char delim[2];
+    delim[0] = a_delim;
+    delim[1] = 0;
+
+    /* Count how many elements will be extracted. */
+    while (*tmp)
+    {
+        if (a_delim == *tmp)
+        {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+
+    /* Add space for trailing token. */
+    count += last_comma < (a_str + strlen(a_str) - 1);
+
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
+
+    result = malloc(sizeof(char*) * count);
+
+    if (result)
+    {
+        size_t idx  = 0;
+        char* token = strtok(a_str, delim);
+
+        while (token)
+        {
+            assert(idx < count);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        assert(idx == count - 1);
+        *(result + idx) = 0;
+    }
+
+    return result;
+}
 
 int main(void)
 {
@@ -14,8 +63,18 @@ int main(void)
 
  fp = popen(command,"r");
  while((fgets(line,  INT_MAX, fp)))
- {
-  printf("%s \n",line);
+ {  char** tokens;
+     
+     tokens = str_split(line, ' ');
+  if (tokens)
+    {
+       printf("Disc     :\n%s", *(tokens + 0)); 
+       printf("\nSize     :\n%s", *(tokens + 1)); 
+       printf("\nUsed     :\n%s", *(tokens + 2)); 
+       printf("\nAvailable:\n%s", *(tokens + 3)); 
+        
+    }
+  //printf("%s \n",line);
  }
 
  return 0;
